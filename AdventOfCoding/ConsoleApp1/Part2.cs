465e8404f27e758582bff42a304d61397fed76f9
Joshua Wegener <joshua.wegener@zeiss.com>
2023-12-01 12:30:52 +0100

    Day1 Part2

---
+++ AdventOfCoding/ConsoleApp1/Part2.cs
@@ -0,0 +1,62 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Day1;
+
+internal static class Part2
+{
+    private static readonly Regex _digitRegex = new(@"(?<=(one|two|three|four|five|six|seven|eight|nine|[0-9]))");
+
+    public static int GetSum(IEnumerable<string> lines)
+    {
+        return lines
+            .Select((line =>
+            {
+                var matches = _digitRegex
+                    .Matches(line);
+
+                var nums = matches
+                    .Select(match => ParseMatch(match))
+                    .Where(num => num != null)
+                    .Cast<int>()
+                    .ToList();
+
+                return GetTwoDigitNumber(nums);
+            }))
+            .Sum();
+    }
+
+    private static int? ParseMatch(Match match)
+    {
+        var value = match.Groups[1].Value;
+
+        if(value.Length == 1)
+        {
+            return int.Parse(value);
+        }
+
+        var digit = value switch
+        {
+            "one" => 1,
+            "two" => 2,
+            "three" => 3,
+            "four" => 4,
+            "five" => 5,
+            "six" => 6,
+            "seven" => 7,
+            "eight" => 8,
+            "nine" => 9,
+            "zero" => 0,
+            _ => null as int?
+        };
+
+        return digit;
+    }
+
+    private static int GetTwoDigitNumber(IReadOnlyList<int> nums)
+    {
+        var firstDigit = nums.FirstOrDefault();
+        var lastDigit = nums.LastOrDefault();
+
+        return int.Parse($"{firstDigit}{lastDigit}");
+    }
+}
--- AdventOfCoding/ConsoleApp1/Program.cs
+++ AdventOfCoding/ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿
 using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
 
 using Day1;
 

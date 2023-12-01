ef926fa4ebd1fca3890293a18b447e78cb662ecc
Joshua Wegener <joshua.wegener@zeiss.com>
2023-12-01 11:26:57 +0100

    Day1 Part1

---
+++ AdventOfCoding/ConsoleApp1/Part1.cs
@@ -0,0 +1,38 @@
+﻿namespace Day1;
+
+internal static class Part1
+{
+    private static int? GetNumbers(char character)
+    {
+        if (int.TryParse(new[] { character }, out var num))
+        {
+            return num;
+        }
+        else
+        {
+            return null as int?;
+        }
+    }
+
+    private static int GetTwoDigitNumber(IEnumerable<int?> nums)
+    {
+        var firstDigit = nums.FirstOrDefault() ?? 0;
+        var lastDigit = nums.LastOrDefault() ?? 0;
+
+        return int.Parse($"{firstDigit}{lastDigit}");
+    }
+
+    public static int GetSum(IEnumerable<string> lines)
+    {
+        return lines
+            .Select((line =>
+            {
+                var nums = line
+                    .Select(GetNumbers)
+                    .Where(num => num != null);
+
+                return GetTwoDigitNumber(nums);
+            }))
+            .Sum();
+    }
+}
---
+++ AdventOfCoding/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Runtime.CompilerServices;
+
+using Day1;
+
+var fileLines = File.ReadAllLines("input.txt");
+
+var sumPart1 = Part1.GetSum(fileLines);
+
+Console.WriteLine($"Sum Part1: {sumPart1}");
+
+var sumPart2 = Part2.GetSum(fileLines);
+
+Console.WriteLine($"Sum Part2: {sumPart2}");

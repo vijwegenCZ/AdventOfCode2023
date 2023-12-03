7b8702a9aa854387b1e63ebc28306569ab0e0782
Joshua Wegener <joshua.wegener@zeiss.com>
2023-12-03 12:09:31 +0100

    Add template

---
+++ AdventOfCode/Template/Part1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template;
+
+internal static class Part1
+{
+    public static object DoStuff(string[] inputLines)
+    {
+        return new object();
+    }
+}

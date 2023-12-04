580d24a3820525a3eee0dc38ab9934db7106f717
Joshua Wegener <joshua.wegener@zeiss.com>
2023-12-04 12:02:17 +0100

    Day4 Part2(uncertain)

--- AdventOfCode/Day4/Part2.cs
+++ AdventOfCode/Day4/Part2.cs
@@ -1,15 +1,72 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Template;
+﻿namespace Template;
 
 internal static class Part2
 {
-    public static object DoStuff(string[] inputLines)
+    public static int GetCountOfScratchCards(string[] inputLines)
     {
-        return new object();
+        var originalCards = inputLines
+            .Select((line, index) => KeyValuePair.Create(index, GetScratchcard(line)))
+            .ToDictionary(pair => pair.Key, pair => pair.Value);
+
+        var originalCardsWinningNumber = new Dictionary<int, int>();
+
+        var resultingCards = originalCards.Count;
+
+        foreach(var originalCard in originalCards)
+        {
+            var totalCountOfNestedCards = 0;
+            var card = originalCard.Value;
+            var cardsToEvaluate = card.RelevantNumbers;
+
+            var nested = cardsToEvaluate.Select(c => originalCards[c]).ToList();
+
+            while (nested.Any())
+            {
+                totalCountOfNestedCards += nested.Count;
+
+                var foo = nested
+                    .SelectMany(s => s.RelevantNumbers)
+                    .Select(s => originalCards[s]);
+
+                nested = foo.ToList();
+            }
+
+            originalCardsWinningNumber[originalCard.Key] = totalCountOfNestedCards;
+        }
+
+        var wins = originalCards
+            .Select(s => originalCardsWinningNumber[s.Key])
+            .Sum();
+
+        return resultingCards + wins;
+    }
+
+    private static Scratchcard GetScratchcard(string line)
+    {
+        var removedMetaData = line.Split(":").Last().Trim().Split("|");
+
+        var winningNumbers = GetNumbers(removedMetaData.First());
+        var existingNumbers = GetNumbers(removedMetaData.Last());
+
+        return new Scratchcard(winningNumbers, existingNumbers);
+    }
+
+    private static IEnumerable<int> GetNumbers(string line)
+    {
+        return line
+                    .Trim()
+                    .Split(" ")
+                    .Where(num => !string.IsNullOrWhiteSpace(num))
+                    .Select(int.Parse);
+    }
+
+    private sealed class Scratchcard
+    {
+        public Scratchcard(IEnumerable<int> winningNumbers, IEnumerable<int> existingNumbers)
+        {            
+            RelevantNumbers = winningNumbers.Intersect(existingNumbers);
+        }
+
+        public IEnumerable<int> RelevantNumbers { get; }
     }
 }
